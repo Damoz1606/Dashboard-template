6c2b511e1209136a537f37bc0f174ba4db4dd8b2
Damoz1606 <panchitodmun@gmail.com>
2023-05-14 22:46:51 -0500

    Remove unused imports

--- src/components/MuiComponentSamples/Samples/BottomNavigation.tsx
+++ src/components/MuiComponentSamples/Samples/BottomNavigation.tsx
@@ -3,6 +3,7 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import RestoreIcon from "@mui/icons-material/Restore";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import React from "react";
 
 export default function BottomNavigationExample() {
   const [value, setValue] = React.useState(0)
